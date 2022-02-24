790d873366ccc4b596cc3c74f12127b760207260
ShallowGreen <183339728@qq.com>
2022-02-24 15:16:58 +0800

    feat: complete contract depoly

--- W1/scripts/deploy.ts
+++ W1/scripts/deploy.ts
@@ -1,25 +1,6 @@
-// We require the Hardhat Runtime Environment explicitly here. This is optional
-// but useful for running the script in a standalone fashion through `node <script>`.
-//
-// When running the script with `npx hardhat run <script>` you'll find the Hardhat
-// Runtime Environment's members available in the global scope.
 import { ethers } from "hardhat";
 
 async function main() {
-  // Hardhat always runs the compile task when running scripts with its command
-  // line interface.
-  //
-  // If this script is run directly using `node` you may want to call compile
-  // manually to make sure everything is compiled
-  // await hre.run('compile');
-
-  // We get the contract to deploy
-  // const [deployer] = await ethers.getSigners();
-  // const accountBalance = await deployer.getBalance();
-
-  // console.log("Deploying contracts with account: ", deployer.address);
-  // console.log("Account balance: ", accountBalance.toString());
-
   const Token = await ethers.getContractFactory("WavePortal");
   const portal = await Token.deploy({
     value: ethers.utils.parseEther("0.1"),
@@ -29,8 +10,6 @@ async function main() {
   console.log("WavePortal address: ", portal.address);
 }
 
-// We recommend this pattern to be able to use async/await everywhere
-// and properly handle errors.
 main().catch((error) => {
   console.error(error);
   process.exitCode = 1;
